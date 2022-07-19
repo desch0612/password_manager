cad053c8cdcf315e4ac2837b11b6fe898e4de907
Schaefer <dennis.schaefer0612@gmail.com>
2022-07-19 13:35:09 +0200

    connection to the Database.

---
+++ connection_database.py
@@ -0,0 +1,22 @@
+# Connection (API) between the database and the password_manager
+
+import sqlite3
+
+# connection (database)
+db_path = "/Users/dennisschafer/lazydata/Passwort_Manager/Datenbank/password_manager/Password_DB.sqbpro"
+db_connection = sqlite3.connect.connect(db_path)
+db_cursor = db_connection.cursor()
+db_cursor.execute("SELECT * FROM user")
+found_data = db_cursor.fetchall()
+
+print(str(found_data))
+
+
+
+
+
+
+
+
+
+
