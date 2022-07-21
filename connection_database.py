9345e6ea3e735d8fc7cd83751658937b505766f5
Schaefer <dennis.schaefer0612@gmail.com>
2022-07-21 11:59:45 +0200

    The test options deleted from the connection_database and some changes in the database tables.

--- connection_database.py
+++ connection_database.py
@@ -4,12 +4,11 @@ import sqlite3
 
 # connection (database)
 db_path = "/Users/dennisschafer/lazydata/Passwort_Manager/Datenbank/password_manager/Password_DB.db"
-db_connection = sqlite3.connect.connect(db_path)
+db_connection = sqlite3.connect(db_path)
 db_cursor = db_connection.cursor()
-db_cursor.execute("SELECT * FROM user")
 found_data = db_cursor.fetchall()
 
-print(str(found_data))
+
 
 
 
