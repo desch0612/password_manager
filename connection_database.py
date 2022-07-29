b4ff47b2ac844d4554491381af45f0444c2fa3bb
Schaefer <dennis.schaefer0612@gmail.com>
2022-07-29 12:55:04 +0200

    add a new todo!

--- connection_database.py
+++ connection_database.py
@@ -1,8 +1,8 @@
-# Connection (API) between the database and the password_manager
-
 import sqlite3
 
-# connection (database)
+# todo: db_path has to save not in a local path!
+
+# Connection (API) between the database and the password_manager
 db_path = "/Users/dennisschafer/lazydata/Passwort_Manager/pw_manager/Password_Manager_DB.db"
 db_connection = sqlite3.connect(db_path)
 db_cursor = db_connection.cursor()
