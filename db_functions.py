b27d5821dc25ded463f1742a2a5b9c337e3b2936
TobDed <t.dedusev@gmx.de>
2022-07-30 21:54:32 +0200

    Change Dictionary-Keys in get_entries()

--- db_functions.py
+++ db_functions.py
@@ -8,18 +8,18 @@ import os  # includes functions to interact with the file system.
 
 
 def get_entries():
-    entry1 = {"id": 1, "name": "Amazon", "pw": "123456"}
-    entry2 = {"id": 2, "name": "ebay", "pw": "test123"}
-    entry3 = {"id": 3, "name": "website1", "pw": "daskjca"}
-    entry4 = {"id": 4, "name": "website2", "pw": "pjpaf"}
-    entry5 = {"id": 5, "name": "website3", "pw": "lcksdnl"}
-    entry6 = {"id": 6, "name": "website4", "pw": "kdjak"}
-    entry7 = {"id": 7, "name": "website4", "pw": "kdjak"}
-    entry8 = {"id": 8, "name": "website4", "pw": "kdjak"}
-    entry9 = {"id": 9, "name": "website4", "pw": "kdjak"}
-    entry10 = {"id": 10, "name": "website4", "pw": "kdjak"}
-    entry11 = {"id": 11, "name": "website4", "pw": "kdjak"}
-    entry12 = {"id": 12, "name": "website4", "pw": "kdjak"}
+    entry1 = {"id": 1, "website": "Amazon", "password": "123456"}
+    entry2 = {"id": 2, "website": "ebay", "password": "test123"}
+    entry3 = {"id": 3, "website": "website1", "password": "daskjca"}
+    entry4 = {"id": 4, "website": "website2", "password": "pjpaf"}
+    entry5 = {"id": 5, "website": "website3", "password": "lcksdnl"}
+    entry6 = {"id": 6, "website": "website4", "password": "kdjak"}
+    entry7 = {"id": 7, "website": "website4", "password": "kdjak"}
+    entry8 = {"id": 8, "website": "website4", "password": "kdjak"}
+    entry9 = {"id": 9, "website": "website4", "password": "kdjak"}
+    entry11 = {"id": 11, "website": "website4", "password": "kdjak"}
+    entry10 = {"id": 10, "website": "website4", "password": "kdjak"}
+    entry12 = {"id": 12, "website": "website4", "password": "kdjak"}
 
     entries = [entry1, entry2, entry3, entry4, entry5, entry6, entry7, entry8, entry9, entry10, entry11, entry12]
     return entries
