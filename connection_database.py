27eed7640579965dc9f6855ac7b356ebbb7975a0
Schaefer <dennis.schaefer0612@gmail.com>
2022-08-05 10:11:28 +0200

    change the path of the database file.

--- connection_database.py
+++ connection_database.py
@@ -1,9 +1,7 @@
 import sqlite3
 
-# todo: db_path has to save not in a local path!
-
 # Connection (API) between the database and the password_manager
-db_path = "/Users/dennisschafer/lazydata/Passwort_Manager/pw_manager/Password_Manager_DB.db"
+db_path = "Password_Manager_DB.db"
 db_connection = sqlite3.connect(db_path)
 db_cursor = db_connection.cursor()
 found_data = db_cursor.fetchall()
