b1249388360c6d9f19c181065d793f3452adc5a0
Schaefer <dennis.schaefer0612@gmail.com>
2022-08-07 17:51:41 +0200

    Comments added.

---
+++ hash_functions.py
@@ -0,0 +1,4 @@
+# Here the functions are to be added that take a hash value and return a plaintext value
+# and the other function takes a plaintext value and returns a hash value.
+# The encryption is to be based on AES-256.
+
