ab7b88bf67550023bef8b12437acf658b7039de4
Schaefer <dennis.schaefer0612@gmail.com>
2022-08-07 19:33:34 +0200

    The function hash_function takes the plaintext values (passwords) and returns the hash values. Currently the hash values are in a dataframe with the other data. But the hash values still have to be stored in the database.
    Additionally, pandas was imported to generate the data frame.

--- hash_functions.py
+++ hash_functions.py
@@ -1,4 +1,26 @@
-# Here the functions are to be added that take a hash value and return a plaintext value
-# and the other function takes a plaintext value and returns a hash value.
-# The encryption is to be based on AES-256.
+'''
+Here the functions are to be added that take a hash value and return a plaintext value
+and the other function takes a plaintext value and returns a hash value.
+he encryption is to be based on sha-256.
+'''
+import hashlib
+import pandas as pd
+import db_functions
+
+# with the dataframe, we load all data from the database into a dataframe.
+df = pd.DataFrame.from_dict(db_functions.fetch_all())
+
+# This function takes a plaintext value (password) and returns the hash value.
+# The hash value is saved as a hexadecimal value.
+def hash_function(password):
+ return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+# hash_value is the new column in the df with the hash_values.
+df['hash_value'] = df['password'].apply(hash_function)
+print(df.head())
+# todo: the hash-value has to be saved in the database.
+
+def plaintext_function(hash_value):
+ pass
+
 
