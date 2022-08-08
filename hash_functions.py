d21054eb67cb99105dfe4c45b31521918e5171a4
Schaefer <dennis.schaefer0612@gmail.com>
2022-08-08 13:49:28 +0200

    Added a todo.

--- hash_functions.py
+++ hash_functions.py
@@ -4,22 +4,17 @@ and the other function takes a plaintext value and returns a hash value.
 he encryption is to be based on sha-256.
 '''
 import hashlib
-import pandas as pd
+import connection_database
 import db_functions
 
-# with the dataframe, we load all data from the database into a dataframe.
-df = pd.DataFrame.from_dict(db_functions.fetch_all())
 
 # This function takes a plaintext value (password) and returns the hash value.
 # The hash value is saved as a hexadecimal value.
 def hash_function(password):
- return hashlib.sha256(password.encode('utf-8')).hexdigest()
+ hash_value = hashlib.sha256(password.encode('utf-8')).hexdigest()
+ return hash_value
 
-# hash_value is the new column in the df with the hash_values.
-df['hash_value'] = df['password'].apply(hash_function)
-print(df.head())
-# todo: the hash-value has to be saved in the database.
-
+# todo: The hash value must not be displayed for the user but his plaintext password. This must still be changed.
 # This function takes a hash-value and returns the plaintext.
 def plaintext_function(hash_value):
  pass
