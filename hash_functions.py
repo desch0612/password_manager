5102aae3a6b26efe3ce26062775a816e7df379d2
Schaefer <dennis.schaefer0612@gmail.com>
2022-08-17 10:21:26 +0200

    import cryptography to decrypt and encrypt the values und create the key.

--- hash_functions.py
+++ hash_functions.py
@@ -3,13 +3,20 @@ Here the functions are to be added that take a hash value and return a plaintext
 and the other function takes a plaintext value and returns a hash value.
 he encryption is to be based on sha-256.
 '''
-import hashlib
+import cryptography
+from cryptography.fernet import Fernet
 
 # This function takes a plaintext value (password) and returns the hash value.
 # The hash value is saved as a hexadecimal value.
 def hash_function(password):
- hash_value = hashlib.sha256(password.encode('utf-8')).hexdigest()
- return hash_value
+    key = Fernet.generate_key()
+    crypter = Fernet(key)
+    pw = crypter.encrypt(b"password")
+    decryptString = crypter.decrypt(pw)
+    print(str(pw, 'utf8'))
+    print(str(decryptString, 'utf8' ))
+
+hash_function("test")
 
 # todo: The hash value must not be displayed for the user but his plaintext password. This must still be changed.
 # This function takes a hash-value and returns the plaintext.
