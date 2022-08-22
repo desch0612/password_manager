d0fea3f7cdeb3257bbc2a5b7a382d837945b2120
Schaefer <dennis.schaefer0612@gmail.com>
2022-08-22 11:32:20 +0200

    create generate_key and load_key. So now we create one key for one password to encrypt and decrypt.

--- hash_functions.py
+++ hash_functions.py
@@ -6,32 +6,33 @@ and the other function takes a plaintext value and returns a hash value.
 from cryptography.fernet import Fernet
 
 # todo: outsource the generate key from the encrypt function. (Then we have the same key to encrypt and decrypt)
-def generate_key():
-    key = Fernet.generate_key()  # generate the key.
-    return key
+# Generates a key and save it into a file
+def generatekey():
+    key = Fernet.generate_key()
+    with open("secret.key", "wb") as key_file:
+        key_file.write(key)
 
-# This function takes a plaintext value (password) and returns the hash value.
-# The hash value is saved as a hexadecimal value.
+# Load the previously generated key
+def load_key():
+    return open("secret.key", "rb").read()
+
+# encrypt the password
 def encrypt(password):
-    key = generate_key()
+    key = load_key()
     encoded_password = password.encode()
     crypter = Fernet(key)
     crypted_string = crypter.encrypt(encoded_password)
     return crypted_string.decode('utf-8') # convert the hash value into a string!
 
-
-
 # todo: The hash value must not be displayed for the user but his plaintext password. This must still be changed.
 # This function takes a hash-value and returns the plaintext.
 def decrypt(crypted_string):
-    key = Fernet.generate_key()
+    key = load_key()
     crypter = Fernet(key)
     crypted_string = crypter.encrypt(bytes(crypted_string, 'utf-8'))
     decrypted_string = crypter.decrypt(crypted_string)
 
-    return decrypted_string
-
-
+    return decrypted_string.decode()
 
 
 
